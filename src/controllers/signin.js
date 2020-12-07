b2275bbfeacdd1383214590b33c6f4392217e079
Edwin PAYELLE <edwinpayelle@me.com>
2020-12-07 12:02:21 +0100

    new update signin

--- src/controllers/signin.js
+++ src/controllers/signin.js
@@ -1,20 +1,28 @@
+const userModel = require('../db/models/user');
+const uid2 = require('uid2');
+const SHA256 = require("crypto-js/sha256");
+var encBase64 = require("crypto-js/enc-base64");
+
+const token = uid2(32)
+
+
 async function signIn(req, res) {
 
     var result = false
     var user = null
     var error = []
   
-    if (req.body.emailFromFront == '' ||
-      req.body.passwordFromFront == ''
+    if (req.body.email == '' ||
+      req.body.password == ''
     ) {
       error.push('champs vides')
     }
     if (error.length == 0) {
       console.log(req.body)
       user = await userModel.findOne({
-        email: req.body.emailFromFront
+        email: req.body.email
       });
-      var hash = SHA256(req.body.passwordFromFront + user.salt).toString(encBase64);
+      var hash = SHA256(req.body.password + user.salt).toString(encBase64);
   
       console.log(hash, user.password)
       if (hash === user.password) {
