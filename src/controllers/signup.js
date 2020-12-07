d78b4eafc43029dface44c2df942557aed707d35
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-07 10:51:14 +0100

    implementation of the token and encryptic the password on the sign up

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,16 +1,22 @@
-const UserModel = require('../db/models/user');
 const uid2 = require('uid2');
+const SHA256 = require("crypto-js/sha256");
+var encBase64 = require("crypto-js/enc-base64");
+
+const UserModel = require('../db/models/user');
+
 
 async function signUp(req, res) {
+
+    const salt = uid2(32);
 
     const newUser = new UserModel({
         firstname: req.body.firstname,
         name: req.body.mail,
         email: req.body.email,
-        password: req.body.password,
+        password: SHA256(req.body.password + salt).toString(encBase64),
         token: uid2(32),
         status: req.body.status,
-        salt: req.body.salt,
+        salt: salt,
         avatar: req.body.avatar
     });
 
