8c56359c88fb35a04406855ebc51f02a0eda2610
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-07 11:45:10 +0100

    catch error if empty area and formating response

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -10,6 +10,19 @@ async function signUp(req, res) {
     const salt = uid2(32);
     let userSaved;
     const errorArray = [];
+    let recorded = false;
+
+    if (req.body.name === undefined || req.body.name === null) {
+        errorArray.push("champ requis name vide")
+    }
+
+    if (req.body.email === undefined || req.body.email === null) {
+        errorArray.push("champ requis email vide")
+    }
+
+    if (req.body.password === undefined || req.body.password === null) {
+        errorArray.push("champ requis password vide")
+    }
 
     try {
         const newUser = new UserModel({
@@ -18,21 +31,22 @@ async function signUp(req, res) {
             email: req.body.email,
             password: SHA256(req.body.password + salt).toString(encBase64),
             token: uid2(32),
-            status: req.body.status,
+            status: "fan",
             salt: salt,
             avatar: req.body.avatar
         });
 
         userSaved = await newUser.save();
+        recorded = true;
+
     } catch (error) {
-        console.log('ERREUR', error.code);
         if (error.code === 11000) {
             errorArray.push("email existant");
         }
     }
 
     res.json({
-        message: "ok",
+        recorded: recorded,
         data: userSaved,
         error: errorArray
     })
