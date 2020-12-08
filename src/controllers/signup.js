de44e94ba88392405c5ce18fbc18019736b84857
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-08 10:28:15 +0100

    payload validation with Joi on sign up

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -18,7 +18,7 @@ const schemaPayload = Joi.object({
 
     email: Joi.string()
         .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-        .required
+        .required()
 })
 
 
@@ -29,38 +29,58 @@ async function signUp(req, res) {
     const errorArray = [];
     let recorded = false;
 
-    if (req.body.name === undefined || req.body.name === null) {
-        errorArray.push("champ requis name vide")
-    }
+    const { error } = schemaPayload.validate(req.body,
+        { abortEarly: false });
 
-    if (req.body.email === undefined || req.body.email === null) {
-        errorArray.push("champ requis email vide")
-    }
-
-    if (req.body.password === undefined || req.body.password === null) {
-        errorArray.push("champ requis password vide")
-    }
+    console.log('ERREUR', error);
 
     try {
-        const newUser = new UserModel({
-            firstname: req.body.firstname,
-            name: req.body.name,
-            email: req.body.email,
-            password: SHA256(req.body.password + salt).toString(encBase64),
-            token: uid2(32),
-            status: "fan",
-            salt: salt,
-            avatar: req.body.avatar
+
+
+        const answerJoi = error.details;
+
+        answerJoi.forEach(element => {
+            errorArray.push(element.message)
         });
-
-        userSaved = await newUser.save();
-        recorded = true;
-
     } catch (error) {
-        if (error.code === 11000) {
-            errorArray.push("email existant");
+        console.log(error);
+    }
+
+
+    // if (req.body.name === undefined || req.body.name === null) {
+    //     errorArray.push("champ requis name vide")
+    // }
+
+    // if (req.body.email === undefined || req.body.email === null) {
+    //     errorArray.push("champ requis email vide")
+    // }
+
+    // if (req.body.password === undefined || req.body.password === null) {
+    //     errorArray.push("champ requis password vide")
+    // }
+    if (errorArray.length === 0) {
+        try {
+            const newUser = new UserModel({
+                firstname: req.body.firstname,
+                name: req.body.name,
+                email: req.body.email,
+                password: SHA256(req.body.password + salt).toString(encBase64),
+                token: uid2(32),
+                status: "fan",
+                salt: salt,
+                avatar: req.body.avatar
+            });
+
+            userSaved = await newUser.save();
+            recorded = true;
+
+        } catch (error) {
+            if (error.code === 11000) {
+                errorArray.push("email existant");
+            }
         }
     }
+
 
     res.json({
         recorded: recorded,
