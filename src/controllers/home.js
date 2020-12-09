ebba3104a5a015de2c528348b4f4bce2a0200a99
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-09 16:13:46 +0100

    algo on the route position for filtering

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,6 +1,10 @@
+
 function home(req, res) {
 
-    res.json({ message: 'Welcome to the Rally server' });
+    res.json({
+        message: 'Welcome to the Rally server',
+
+    });
 }
 
 module.exports = home;
--- src/controllers/transmitVehiculePosition.js
+++ src/controllers/transmitVehiculePosition.js
@@ -1,8 +1,28 @@
 //// This function relay the position of a vehicule from transponder toward frontend ////
 
+let allPosition = [];
+
 function transmitVehiculePosition(req, res) {
-    console.log('POSITION', req.body);
-    req.dependencies.socketServer.emit('sendPositionToAll', { positions: req.body });
+    console.log('DATA', req.body);
+
+    let alreadyRecorded = false;
+
+
+    for (let i = 0; i < allPosition.length; i++) {
+        if (allPosition[i].idVehicule === req.body.idVehicule) {
+            allPosition[i] = req.body;
+            alreadyRecorded = true;
+        }
+
+    }
+
+    if (!alreadyRecorded) {
+        allPosition.push(req.body)
+    }
+
+    console.log('POSITION', allPosition);
+
+    req.dependencies.socketServer.emit('sendPositionToAll', { allPosition });
     res.json(req.body)
 }
 
