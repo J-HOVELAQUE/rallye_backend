84321a1d2a7ceaf5f5a35ecc893fa189d7a37703
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-09 17:03:40 +0100

    sending data position for each vehicule in web socket

--- src/controllers/transmitVehiculePosition.js
+++ src/controllers/transmitVehiculePosition.js
@@ -3,25 +3,16 @@
 let allPosition = [];
 
 function transmitVehiculePosition(req, res) {
-    console.log('DATA', req.body);
-
     let alreadyRecorded = false;
-
-
     for (let i = 0; i < allPosition.length; i++) {
         if (allPosition[i].idVehicule === req.body.idVehicule) {
             allPosition[i] = req.body;
             alreadyRecorded = true;
         }
-
     }
-
     if (!alreadyRecorded) {
         allPosition.push(req.body)
     }
-
-    console.log('POSITION', allPosition);
-
     req.dependencies.socketServer.emit('sendPositionToAll', { allPosition });
     res.json(req.body)
 }
