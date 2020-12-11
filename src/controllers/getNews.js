06422b7d89b3a90227bd3558098405bbd298711a
Stéphanie Bézard <steph.bezard@gmail.com>
2020-12-11 15:24:46 +0100

    route get en cours...

--- src/controllers/getNews.js
+++ src/controllers/getNews.js
@@ -1,7 +1,16 @@
 const NewsModel = require('../db/models/news');
 
 async function getNews(req, res) {
-    
+    const newsDb = await NewsModel({ 
+        title: req.query.title,
+        image: req.query.image,
+        description: req.query.description,
+        date: req.query.date,
+    });
+
+    console.log('LES NEWS', newsDb);
+
+    res.json({ news: newsDb })
 }
 
 module.exports = getNews;
