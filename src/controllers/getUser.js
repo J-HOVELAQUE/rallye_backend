b98876a8b1655540dacc3629d5e21d15b94a41d6
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-11 12:20:07 +0100

    modification of get user controller for populate

--- src/controllers/getUser.js
+++ src/controllers/getUser.js
@@ -2,8 +2,10 @@ const UserModel = require('../db/models/user');
 
 async function getUser(req, res) {
     console.log('QUERY', req.query.token);
-    const answerDb = await UserModel.findOne({ token: req.query.token });
-    console.log('USER', answerDb);
+    const answerDb = await UserModel.findOne({ token: req.query.token })
+        .populate('favorites')
+        .exec();
+
 
     res.json({
         user: answerDb
