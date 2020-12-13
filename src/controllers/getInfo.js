9e898a4dea38acd3bb3746d7808c9de78927c2d8
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-13 15:20:57 +0100

    the route getInfo now send the accomodation of the 09/02 with the token of the query

--- src/controllers/getInfo.js
+++ src/controllers/getInfo.js
@@ -1,26 +1,27 @@
 const AccomodationModel = require('../db/models/accomodation');
+const mongoose = require('mongoose');
+
+const getIdWithToken = require('../tools/getIdWithToken');
 
 async function getInfo(req, res) {
 
-
+    const idUser = await getIdWithToken(req.query.token);
 
     const dataFounded = await AccomodationModel
 
         .aggregate()
-        .lookup({
-            'from': 'users',
-            'localField': 'users',
-            'foreignField': '_id',
-            'as': 'usersData'
-        })
-        .addFields({ "month": { $month: '$date' } })
-        .match({ month: 9 })
-        // .match({ "userData.firstname": "Romain" })
+        // .lookup({
+        //     'from': 'users',
+        //     'localField': 'users',
+        //     'foreignField': '_id',
+        //     'as': 'usersData'
+        // })
+        .addFields({ "month": { $month: '$date' }, "day": { $dayOfMonth: '$date' } })
+        .match({ day: 2 }, { month: 9 })
+        .match({ users: mongoose.Types.ObjectId(idUser) })
+
+        // .match({ userData: { firstname: 'Romain' } })
         .exec()
-
-    console.log("Nombre de match", dataFounded.length);
-
-
 
     // const dataFounded = await aggr.exec();
     // const dataFounded = await AccomodationModel.find({ users: '5fd218e245c4df00171790c5' })
