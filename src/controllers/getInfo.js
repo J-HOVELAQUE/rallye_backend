6814a31a5426cfd4910f9f51be192fed94a73880
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-13 11:00:25 +0100

    get accomodation with the date and populate it on getInfo

--- src/controllers/getInfo.js
+++ src/controllers/getInfo.js
@@ -1,7 +1,31 @@
 const AccomodationModel = require('../db/models/accomodation');
 
 async function getInfo(req, res) {
-    res.json({ message: "information" })
+
+
+
+    const dataFounded = await AccomodationModel
+
+        .aggregate()
+        .lookup({
+            'from': 'users',
+            'localField': 'users',
+            'foreignField': '_id',
+            'as': 'usersData'
+        })
+        .addFields({ "month": { $month: '$date' } })
+        .match({ month: 9 })
+        // .match({ "userData.firstname": "Romain" })
+        .exec()
+
+    console.log("Nombre de match", dataFounded.length);
+
+
+
+    // const dataFounded = await aggr.exec();
+    // const dataFounded = await AccomodationModel.find({ users: '5fd218e245c4df00171790c5' })
+
+    res.json({ message: dataFounded })
 }
 
 module.exports = getInfo;
