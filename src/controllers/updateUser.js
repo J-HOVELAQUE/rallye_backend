6a244069558074bd0269852108e7236ff2c0d387
Manon Le <babyvert13@hotmail.com>
2020-12-14 12:05:24 +0100

    modification updateUser route

--- src/controllers/updateUser.js
+++ src/controllers/updateUser.js
@@ -3,13 +3,28 @@ const UserModel = require('../db/models/user');
 async function updateUser(req, res) {
 
     let result = false
-    var update = { [req.body.keyToUpdate]: req.body.newValue }
+    // var update = { [req.body.keyToUpdate]: req.body.newValue }
+    let jsonFields = JSON.parse(req.body.newValue)
+
+    var update = {
+        firstname: jsonFields.userFirstName,
+        name: jsonFields.userLastName,
+        email: jsonFields.userEmail,
+        avatar: jsonFields.userAvatar,
+        nationality: jsonFields.userNationality
+    }
+
+    console.log('RAW ////// : ', req.body.newValue)
+    console.log('PARSE /////// : ', jsonFields)
+    console.log('UPDATE ////// : ', update)
 
     const updateDb = await UserModel.updateOne(
         {
             token: req.body.token
         }, update
     )
+
+    console.log(updateDb)
     if (updateDb.nModified === 1) {
         result = true
     }
