b9bc492412e9127dca654d77bfe939529590e16a
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-14 08:34:32 +0100

    the get-info route now return the accomodation for the current day

--- src/controllers/getInfo.js
+++ src/controllers/getInfo.js
@@ -1,32 +1,44 @@
-const AccomodationModel = require('../db/models/accomodation');
 const mongoose = require('mongoose');
+
+const AccomodationModel = require('../db/models/accomodation');
+const CateringModel = require('../db/models/catering');
 
 const getIdWithToken = require('../tools/getIdWithToken');
 
 async function getInfo(req, res) {
 
     const idUser = await getIdWithToken(req.query.token);
+    const today = new Date;
 
-    const dataFounded = await AccomodationModel
+    console.log('MONTH', today.getMonth());
+    console.log('DAY', today.getDate());
+    console.log('YEAR', today.getFullYear());
 
+    console.log("AUJOURD'HUI", today);
+
+    //// Getting the accomodation of the day for the user connected (with is token) /////
+    const accomodation = await AccomodationModel
         .aggregate()
+        .match({ users: mongoose.Types.ObjectId(idUser) })
+
         // .lookup({
         //     'from': 'users',
         //     'localField': 'users',
         //     'foreignField': '_id',
         //     'as': 'usersData'
         // })
-        .addFields({ "month": { $month: '$date' }, "day": { $dayOfMonth: '$date' } })
-        .match({ day: 2 }, { month: 9 })
-        .match({ users: mongoose.Types.ObjectId(idUser) })
+        .addFields({ "month": { $month: '$date' }, "day": { $dayOfMonth: '$date' }, "year": { $year: '$date' } })
+        .match({ day: today.getDate() }, { month: (today.getMonth() + 1) }, { year: today.getFullYear() })
 
         // .match({ userData: { firstname: 'Romain' } })
         .exec()
 
-    // const dataFounded = await aggr.exec();
-    // const dataFounded = await AccomodationModel.find({ users: '5fd218e245c4df00171790c5' })
 
-    res.json({ message: dataFounded })
+    //// Getting the catering of the day for the user connected /////
+    // const catering = await CateringModel
+
+
+    res.json({ accomodation })
 }
 
 module.exports = getInfo;
