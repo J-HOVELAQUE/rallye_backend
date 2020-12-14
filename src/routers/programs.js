b99a402e12d72cd51ff9a04e81c5b7a658536785
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-14 16:30:50 +0100

    add a route for get program

--- src/routers/programs.js
+++ src/routers/programs.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const getProgram = require('../controllers/getProgram');
 
-router.get('/get-Program', getProgram);
+router.get('/get-program', getProgram);
 
 module.exports = router;
