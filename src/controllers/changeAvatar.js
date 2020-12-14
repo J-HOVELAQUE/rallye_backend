8ab306c725d5b64fa3c63a4693f6794f2cac7705
J-HOVELAQUE <j.hovelaque@orange.fr>
2020-12-14 17:29:51 +0100

    adding and plug a route for changing avatar

---
+++ src/controllers/changeAvatar.js
@@ -0,0 +1,9 @@
+//// A controller that record a photo in cloudinary and bind it to user via token  ////
+
+async function changeAvatar(req, res) {
+
+    console.log("check");
+    res.json({ message: "check" })
+}
+
+module.exports = changeAvatar;
--- src/routers/user.js
+++ src/routers/user.js
@@ -8,6 +8,7 @@ const updateUser = require('../controllers/updateUser');
 const addFavorite = require('../controllers/addFavorite');
 const removeFavorite = require('../controllers/removeFavorite');
 const getInfo = require('../controllers/getInfo');
+const changeAvatar = require('../controllers/changeAvatar');
 
 router.post('/sign-up', signUp);
 router.post('/sign-in', signIn);
@@ -18,5 +19,7 @@ router.put('/add-favorite', addFavorite);
 router.put('/remove-favorite', removeFavorite);
 
 router.get('/get-info', getInfo);
+
+router.post('/change-avatar', changeAvatar);
 
 module.exports = router;
